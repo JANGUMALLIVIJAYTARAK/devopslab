20c51fd95f5fccb8bbc374c0422375068474ed44
vijaytarak <=>
2025-03-21 19:34:04 +0530

    Initial commit

--- app.py
+++ app.py
@@ -4,20 +4,20 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "This is VIjay Tarak Jangumalli"
+    return "This is Vijay Tarak Jangumalli"
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     if request.method == 'POST':
-        name = request.form.get('username')
+        name = request.form.get('name')
         email = request.form.get('email')
         password = request.form.get('password')
 
         # Debug print
         print(f"Name: {name}, Email: {email}, Password: {password}")
 
-        return render_template("Success.html")  
-    return render_template("Register.html") 
+        return render_template("Success.html")  # Render success page
+    return render_template("Register.html")  # Render registration form
 
 if __name__ == '__main__':
     app.run(debug=True)
